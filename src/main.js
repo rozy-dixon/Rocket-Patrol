9b6c5938e9e9979303ef2fe4397e3a279ad1a4f3
Rosalind Dixon <romdixon@ucsc.edu>
2024-01-18 19:14:53 -0800

    asking about errors in section

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ let config = {
     scene: [ Menu, Play ]
 }
 let game = new Phaser.Game(config)
+// reserve keyboard bindings
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT
 // set UI sizes
 let borderUISize = game.config.height / 15
 let borderPadding = borderUISize / 3
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -13,9 +13,17 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0) // white border bottom
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0) // white border left
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0) // white border right
+        // add rocket (p1)
+        this.p1rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0)
+        // define keys
+        keyFIRE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F)
+        keyRESET = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
     }
 
     update() {
-        this.starflied.tilePositionX -= 1
+        this.starflied.tilePositionX -= 4
+        //this.p1Rocket.update()
     }
 }
