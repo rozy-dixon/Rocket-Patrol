a36f07b7fe3030c5c1bcf0daf541bbb801fa509a
Rosalind Dixon <romdixon@ucsc.edu>
2024-01-18 21:41:39 -0800

    being careful

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -29,6 +29,13 @@ class Rocket extends Phaser.GameObjects.Sprite {
         if(this.y <= borderUISize * 3 + borderPadding) {
             this.isFiring = false
             this.y = game.config.height - borderUISize - borderPadding
+            // function call instead???
         }
+    }
+
+    // reset rocket to "ground"
+    reset() {
+        this.isFiring = false
+        this.y = game.config.height - borderUISize - borderPadding
     }
 }
